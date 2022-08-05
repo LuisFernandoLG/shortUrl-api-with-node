74265974ea9ce3ad56751c250560c77164c63bde
Luis <luislopez1099lg@gmail.com>
2022-08-05 12:22:51 -0600

    repass removed

--- middlewares/validatorManager.js
+++ middlewares/validatorManager.js
@@ -4,25 +4,13 @@ import { validateResults } from '../middlewares/validateResults.js'
 
 export const registerBodyValidation = [
   body('email', 'Formato de email incorrecto').isEmail().normalizeEmail(),
-  body('password', 'Formato de contraseña incorrecto')
-    .isLength({ min: 6 })
-    .custom((value, { req }) => {
-      if (value !== req.body.repassword)
-        throw new Error('No coinciden las contraseñas')
-      return value
-    }),
+  body('password', 'Formato de contraseña incorrecto').isLength({ min: 6 }),
   validateResults,
 ]
 
 export const loginBodyValidation = [
   body('email', 'Formato de email incorrecto').isEmail().normalizeEmail(),
-  body('password', 'Formato de contraseña incorrecto')
-    .isLength({ min: 6 })
-    .custom((value, { req }) => {
-      if (value !== req.body.repassword)
-        throw new Error('No coinciden las contraseñas')
-      return value
-    }),
+  body('password', 'Formato de contraseña incorrecto').isLength({ min: 6 }),
   validateResults,
 ]
 
