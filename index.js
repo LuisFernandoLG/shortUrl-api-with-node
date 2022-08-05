bd6cbe1f3f5662d437be26d07d4315bc974dd183
Luis <luislopez1099lg@gmail.com>
2022-08-04 22:27:05 -0600

    cors added

--- index.js
+++ index.js
@@ -5,13 +5,28 @@ import express from 'express'
 import authRouter from './routes/auth.route.js'
 import linkRouter from './routes/link.route.js'
 import cookieParser from 'cookie-parser'
+import cors from 'cors'
 
 const app = express()
-// const apiV1 = "/api/v1"
+
 // Middlewares
+
+const whiteList = [process.env.ORIGIN1]
+
+// De esta manera de llegan a los controladores
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (whiteList.includes(origin)) return callback(null, origin)
+      return callback(`Error de cors, origin : ${origin} no autorizado`, origin)
+    },
+  })
+)
+
 app.use(cookieParser())
 app.use(express.json())
 
+// const apiV1 = "/api/v1"
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/links', linkRouter)
 
